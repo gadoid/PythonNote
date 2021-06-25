e8403c9d7c6a974cf7dba58a1db8bbba20950f31
gadoid <j819902614@live.com>
2021-06-25 22:02:30 +0800

    day2

---
+++ Day2.py
@@ -0,0 +1,12 @@
+# 条件判断
+value = input("value:")
+if value=="a" :
+    print("a")
+elif value != "b":
+    print("c")
+elif value != "a":
+    print("not a")
+else:
+    print("all will fine")
+
+
---
+++ practice2.py
@@ -0,0 +1,12 @@
+#! coding=utf-8
+# grade=input("输入成绩：")
+# grade=int(grade)
+# if grade >= 90 :
+#     print("A")
+# elif grade >=80 :
+#     print("B")
+# elif grade >=70 :
+#     print("C")
+# else :
+#     print("D")
+
