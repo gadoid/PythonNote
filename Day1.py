7920839d152b1afaf9d534f06ef8b856579c42ba
gadoid <j819902614@live.com>
2021-06-25 08:00:20 +0800

    day2 basic info

--- Day1.py
+++ Day1.py
@@ -51,4 +51,79 @@ print(bool(" "),bool(None),bool(""))
 # 运算式结果
 print(bool(1>2),bool(1<2))
 
+# 变量命名
+# 支持的元素 大小写字母、数字、下划线 不能用数字开头
+# 大小写敏感 不要和自带关键子冲突
 
+# 驼峰命名法
+def ApplePie():
+    print("This is a apple pie")
+ApplePie()
+# 小驼峰命名法
+def applePie():
+    print("This is a apple pie")
+applePie()
+
+#PEP8
+#  用小写字母拼写，多个单词用下划线连接
+#  受保护的实例属性用单个下划线开头
+#  私有的实例属性用两个下划线开头
+
+# Python 变量声明
+# Python_variable
+Python_variable="variable"
+print(Python_variable)
+
+#Type函数检查变量类型
+print(type(Python_variable))
+Python_variable=1
+print(type(Python_variable))
+
+#变量运算
+Python_int=8
+Python_float=8.1
+print(Python_float+Python_int)
+Python_str="a"
+try:
+    print(Python_int+Python_str)
+except TypeError:
+    print("TypeError")
+
+#强制转换函数
+# float()
+# str()
+# chr()
+# int()
+# bool()
+# ord()
+print(ord("h"))
+print(chr(0o150))
+
+#变量传值
+content="通过%+类型传递参数"
+print("%s"%(content))
+# 通过format函数
+content="通过format函数"
+#print("{content}".format())
+#简化
+print(f"{content}")
+#直接使用变量连结
+content="直接使用变量连接"
+print("print"+content)
+
+#运算符
+# 切片,左闭右开[]
+list1= [1,2,3,4,5] 
+list2=list1
+list3=list1[:]
+print(id(list1),id(list2),id(list3))
+import copy
+list4=copy.deepcopy(list1)
+print(id(list4))
+#切片参数 [:,2,-1]
+print(list1[-1::1])
+
+#乘方
+print(2**2)
+
+print(-ord('h'))
