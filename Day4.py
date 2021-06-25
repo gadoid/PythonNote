da0c9d57f8b2dee9ee06f6d2444b30464a90d601
gadoid <j819902614@live.com>
2021-06-25 23:41:28 +0800

    day4

---
+++ Day4.py
@@ -0,0 +1,13 @@
+# list1= []
+# for i in range (1,100000):
+#     i=str(i)
+#     m=0
+#     il=len(i)
+#     for j in range (il):
+#         o=int(i[j])**4
+#         m+=o
+#     if m==int(i):
+#         list1.append(m)
+    
+# print(list1)
+
